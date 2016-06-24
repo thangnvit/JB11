166674d9c19afdeca1f0df27dad61a79b8aea36b
DEV <thang6766@gmail.com>
2016-06-24 23:19:15 +0700

    Second Commit

--- src/org/thangnv/Main.java
+++ src/org/thangnv/Main.java
@@ -3,6 +3,6 @@ package org.thangnv;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Từ lần sau chỉ cần commit và push như bình thường");
     }
 }
