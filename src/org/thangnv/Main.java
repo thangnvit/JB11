08d58bab9b142a54bc9cb35099bbca738799c84f
DEV <thang6766@gmail.com>
2016-06-24 23:21:39 +0700

    Three Commit

--- src/org/thangnv/Main.java
+++ src/org/thangnv/Main.java
@@ -3,6 +3,6 @@ package org.thangnv;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Từ lần sau chỉ cần commit và push như bình thường");
+        System.out.println("ok");
     }
 }
